d154487e238ecfbf7753ca4e0d2da29ebc289e22
Lynn-314 <72400896+Lynn-314@users.noreply.github.com>
2021-10-08 13:55:36 +0530

    Update utils.py

--- recog_mnist/mlops2/utils.py
+++ recog_mnist/mlops2/utils.py
@@ -8,10 +8,12 @@ from sklearn import datasets, svm, metrics
 from numpy.lib.function_base import average
 
 
-def preprocess_split_data(digits,split_train,split_val):
-  data=digits.images
+def preprocess_split_data(digits,train_split,val_split,test_split):
+  data=digits["data"]
   n_samples = len(data)
   data = data.reshape((n_samples, -1))
+  split_train = np.round(1 - train_split/100,2)
+  split_val = test_split/(val_split + test_split)
   X_train, X_test, y_train, y_test = train_test_split(
       data, digits.target, test_size=split_train, shuffle=False)
   X_validation, X_test, y_validation, y_test = train_test_split(
@@ -33,6 +35,18 @@ def eval(classifier,x,y):
 def get_random_acc(y):
     return max(np.bincount(y))/len(y)
 
+def run_classification_experiement(x_train,y_train,x_valid,y_valid,gamma,output_model_file,skip_random=True):
+    random_val_acc=get_random_acc(y_valid)
+    clf = svm.SVC(gamma=gamma)
+    clf.fit(x_train,y_train)
+    metrics_valid = eval(clf,x_valid,y_valid)
+    if skip_random and metrics_valid["accuracy"] < random_val_acc:
+        return None
+    output_folder=os.path.dirname(output_model_file)
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+    dump(clf,output_model_file)
+    return metrics_valid
 def run_classification_experiement(x_train,y_train,x_valid,y_valid,gamma,output_model_file,skip_random=True):
     random_val_acc=get_random_acc(y_valid)
     clf = svm.SVC(gamma=gamma)
